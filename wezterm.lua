9071b60fd65b6af5a21217fd9fc4dcbb895e716e
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2024-08-27 07:54:58 +0200

    feat: add only and rotate win

--- wezterm.lua
+++ wezterm.lua
@@ -245,23 +245,21 @@ config.keys = {
       act.SendKey { key = 'o', mods = '' },
     },
   },
-  -- { key = '{', mods = 'CMD|SHIFT',
-  --   action = act.Multiple {
-  --     act.SendKey { key = 'b', mods = 'CTRL' },
-  --     act.SendKey { key = 'i', mods = '' },
-  --   },
-  -- },
-  -- { key = '}', mods = 'CMD|SHIFT',
-  --   action = act.Multiple {
-  --     act.SendKey { key = 'b', mods = 'CTRL' },
-  --     act.SendKey { key = 'o', mods = '' },
-  --   },
-  -- },
+  { key = '{', mods = 'CMD|SHIFT',
+    action = act.Multiple {
+      act.SendKey { key = 'w', mods = 'CTRL' },
+      act.SendKey { key = 'r', mods = '' },
+    },
+  },
+  { key = '}', mods = 'CMD|SHIFT',
+    action = act.Multiple {
+      act.SendKey { key = 'w', mods = 'CTRL' },
+      act.SendKey { key = 'o', mods = 'CTRL' },
+    },
+  },
   -- Turn off the default CMD-m Hide action, allowing CMD-m to
   -- be potentially recognized and handled by the tab
   { key = 'm', mods = 'CMD', action = act.DisableDefaultAssignment },
-  { key = '{', mods = 'CMD|SHIFT', action = act.DisableDefaultAssignment },
-  { key = '}', mods = 'CMD|SHIFT', action = act.DisableDefaultAssignment },
   { key = 'w', mods = 'CMD|CTRL|SHIFT', action = act.CloseCurrentPane { confirm = false },},
   { key = 't', mods = 'CMD|CTRL|SHIFT', action = act.SpawnTab 'CurrentPaneDomain' },
   { key = 'n', mods = 'CMD|CTRL|SHIFT', action = act.SpawnWindow },
